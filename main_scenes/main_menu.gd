c2feae038577a91aeb874963b7a7915dafeabdf8
Arthur Mazzardo Naue <133718057+ArthurNaue@users.noreply.github.com>
2026-06-30 19:55:13 -0300

    enciclopedia funcionando

--- main_scenes/main_menu.gd
+++ main_scenes/main_menu.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-func _on_play_button_pressed():
+func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_scenes/main.tscn")
 
-func _on_enciclopedia_button_pressed():
+func _on_enciclopedia_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_scenes/enciclopedia/enciclopedia.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
