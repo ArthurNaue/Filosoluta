897cc91d25bb08cdc44926772606c7f2fe471c87
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2026-09-01 02:32:38 -0300

    removi o botao de sair

--- main_scenes/main_menu/main_menu.gd
+++ main_scenes/main_menu/main_menu.gd
@@ -5,6 +5,3 @@ func _on_play_button_pressed():
 
 func _on_enciclopedia_button_pressed():
 	get_tree().change_scene_to_file("res://main_scenes/enciclopedia/enciclopedia.tscn")
-
-func _on_button_pressed():
-	get_tree().quit()
